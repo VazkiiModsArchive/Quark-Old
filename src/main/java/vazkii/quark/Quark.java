51307bec2464093349f8278970355e0758524460
Lizbeth Rika <vazkii@hotmail.com>
2015-06-24 23:41:43 +0100

    Initial stuff.

---
+++ src/main/java/vazkii/quark/Quark.java
@@ -0,0 +1,9 @@
+package vazkii.quark;
+
+import vazkii.quark.lib.LibMisc;
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = LibMisc.MOD_ID, name = LibMisc.MOD_NAME, version = LibMisc.VERSION)
+public class Quark {
+
+}
---
+++ src/main/java/vazkii/quark/lib/LibMisc.java
@@ -0,0 +1,9 @@
+package vazkii.quark.lib;
+
+public class LibMisc {
+
+	public static final String MOD_ID = "Quark";
+	public static final String MOD_NAME = MOD_ID;
+	public static final String VERSION = "1.0";
+	
+}
